714cbdc5900ca0a78329575b3b16287504f9b8a1
SantiagoMendez1 <ksantiagomendez01@gmail.com>
2024-05-02 00:30:38 -0500

    conection to db with env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,29 @@ import { AlertModule } from './alert/alert.module';
 import { DeviceDataModule } from './device-data/device-data.module';
 import { SeizureDataModule } from './seizure-data/seizure-data.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development.local',
+      isGlobal: true,
+    }),
     AlertModule,
     DeviceDataModule,
     SeizureDataModule,
-    MongooseModule.forRoot('mongodb://localhost:27017', {
-      dbName: 'tesis-db-test',
+    // MongooseModule.forRoot(
+    //   `mongodb:${process.env.DATABASE_HOST}//:${process.env.DATABASE_PORT}`,
+    //   {
+    //     dbName: process.env.DATABASE_NAME,
+    //   },
+    // ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('DATABASE_URI'),
+      }),
     }),
   ],
   controllers: [AppController],
