723332609f46afcd4130b51cd0f927434a90788f
SantiagoMendez1 <ksantiagomendez01@gmail.com>
2024-05-02 00:32:17 -0500

    init auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
+import { User, UserSchema } from './schemas/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
---
+++ src/auth/schemas/user.schema.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+
+@Schema()
+export class ContactsUser {
+  @Prop({ required: true, lowercase: true })
+  name: string;
+
+  @Prop({ required: true })
+  numberPhone: string;
+}
+export const ConstactUserschema = SchemaFactory.createForClass(ContactsUser);
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true, lowercase: true })
+  name: string;
+
+  @Prop({ unique: [true, 'Duplicate email'] })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ _id: false, type: [ContactsUser] })
+  contacts: Types.Array<ContactsUser>;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
