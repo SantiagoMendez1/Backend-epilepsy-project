2ea1690d7124cbc01594d9a3aa8285444d87d45e
SantiagoMendez1 <ksantiagomendez01@gmail.com>
2024-06-12 12:23:26 -0500

    enable cors socket

--- src/dashboard/dashboard.gateway.ts
+++ src/dashboard/dashboard.gateway.ts
@@ -1,7 +1,13 @@
 import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+    methods: ['GET'],
+    credentials: true,
+  },
+})
 export class DashboardGateway {
   @WebSocketServer()
   server: Server;
